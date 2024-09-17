8cdaf62e4ea2a4bc1b1b3b4245ceebd349d90dc3
Joseph <joseph.burley.1992@gmail.com>
2024-09-17 10:52:38 -0400

    Reads from file and starts cron jobs.

--- src/cron_tab_wrapper.rs
+++ src/cron_tab_wrapper.rs
@@ -1,5 +1,36 @@
-use cron_tab;
+use std::error::Error;
+//use core::marker::Send;
+use cron_tab::*;
+use chrono::{Offset, TimeZone, Utc};
+//use chrono::offset::Offset;
+//use chrono::offset::TimeZone::Offset;
+use crate::Job;
 
-pub fn create_job (job: Job) -> Result<Cron, Box<dyn Error>> {
-    return Result::Err();
+//let utc_tz = Utc;
+
+
+pub fn create_job(job: Job, cron: &mut Cron<Utc>) {
+    cron.add_fn(&job.timing, move || execute_job(&job.params.path)).unwrap();
+}
+
+/*
+pub fn create_job<T>(job: Job, cron: &mut Cron<T>) where
+T: TimeZone + Send + Sync + 'static,
+T::Offset: Send, {
+    cron.add_fn(&job.timing, move || execute_job(&job.params.path)).unwrap();
+}
+*/
+
+
+/*
+pub fn create_job (job: Job) -> Result<Cron<Utc>> {
+    let mut c = Cron::new(Utc);
+    c.add_fn(job.timing.as_str(), move || execute_job(&job.params.path))?;
+    return Result::Err(cron_tab::CronError::Unknown);
+    //return Result::Err("This is supposed to fail");
+}
+    */
+
+fn execute_job(command: &str) {
+    println!("The command is: {}", command);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod user_jobs;
+mod cron_tab_wrapper;
+use cron_tab_wrapper::create_job;
 use user_jobs::*;
 use directories::ProjectDirs;
 use serde_yaml_ng::*;
 use std::fs::{self, File};
 use std::io::Write;
 use std::path::{Path, PathBuf};
+use cron_tab::*;
+use chrono::{FixedOffset, Local, TimeZone, Utc};
 
 fn main() {
     println!("Hello, world!");
@@ -14,7 +18,7 @@ fn main() {
     let job_1 = Job {name: String::from("List Documents"), timing: String::from("* * * * * * *"), params: params_1};
 
     let params_2 = JobParams {path: String::from("ls ~/videos")};
-    let job_2 = Job {name: String::from("List Videos"), timing: String::from("*******"), params: params_2};
+    let job_2 = Job {name: String::from("List Videos"), timing: String::from("* * * * * * *"), params: params_2};
 
     let job_list = JobList {jobs: vec![job_1, job_2]};
 
@@ -41,4 +45,15 @@ fn main() {
     let new_jobs: JobList = serde_yaml_ng::from_str(&input).unwrap();
     println!("The jobs from the file:\n{:?}", new_jobs);
     println!("The first job:\n{:?}", new_jobs.jobs[0]);
+
+    println!("creating crontabs");
+    let mut cron = cron_tab::Cron::new(Utc);
+    for j in new_jobs.jobs {
+        create_job(j, &mut cron);
+    }
+    //println!("Job list maybe?\n{:?}", cron_job_list);
+
+    cron.start();
+    std::thread::sleep(std::time::Duration::from_secs(20));
+    cron.stop();
 }
