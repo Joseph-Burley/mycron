c33b5d6ad51b6fa068be1b39e4ca2e488853cbe0
Joseph <joseph.burley.1992@gmail.com>
2024-10-19 19:38:40 -0400

    now records pid to file. Will not start if pid file exists.

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,26 @@ use directories::ProjectDirs;
 use std::fs::{self, File};
 use std::path::PathBuf;
 use std::sync::mpsc;
+use std::process;
 use chrono:: Utc;
 use mycron::file_watcher::start_watch;
 use simplelog::*;
 #[macro_use] extern crate log;
 
 fn main() {
+    println!("The current process is: {}", process::id());
+    
     let data_dir = ProjectDirs::from("com", "mycron", "mycron").unwrap();
+
+    let mut pid_path = PathBuf::from(data_dir.data_dir());
+    pid_path.push(".pid");
+    if pid_path.exists() {
+        let pid = fs::read_to_string(pid_path).unwrap();
+        println!("Process is already running on pid: {}", pid);
+        return;
+    }
+    let _ = fs::write(&pid_path, format!("{}\n", process::id()));
+    
     let mut log_path = PathBuf::from(data_dir.data_dir());
     log_path.push("mycron_log.log");
     let log_file = File::options().append(true).create(true).open(log_path).unwrap();
@@ -38,13 +51,18 @@ fn main() {
 
 
     let (tx, rx) = mpsc::channel::<u32>();
-
+    let other_tx = tx.clone();
     start_watch(&file_path, tx);
-    
 
     let mut cron = cron_tab::Cron::new(Utc);
     let mut job_handles: Vec<usize> = Vec::new();
-    loop {
+    let mut should_continue = true;
+    ctrlc::set_handler(move || {
+        other_tx.send(1).unwrap();
+        let _ = fs::remove_file(&pid_path);
+    })
+    .expect("Error setting Ctrl-C handler");
+    while should_continue {
         println!("Starting cron");
 
         debug!("started reading from file");
@@ -78,11 +96,16 @@ fn main() {
                         debug!("removing job: {}", i);
                         cron.remove(i);
                     }
+                } else if val == 1 {
+                    should_continue = false;
                 }
             },
             Err(e) => {
-                println!("got erroe: {:?}", e);
+                println!("got error: {:?}", e);
             }
         }
     }
+
+    cron.stop();
+
 }
