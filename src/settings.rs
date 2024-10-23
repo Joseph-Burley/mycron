417b454f70e5a753c3e9f71f68bef587e6226c27
Joseph <joseph.burley.1992@gmail.com>
2024-10-22 21:30:41 -0400

    Added getters and setters to settings module. Logging utility now pulls from settings for config.

--- src/settings.rs
+++ src/settings.rs
@@ -1,6 +1,11 @@
+use clap::builder::Str;
 use directories::ProjectDirs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::fs;
+use std::error::Error;
+use serde::{Serialize, Deserialize};
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Settings {
     pub system_log: String,
     pub job_log: String,
@@ -20,5 +25,102 @@ impl Default for Settings {
             job_log: job_log_path.into_os_string().into_string().unwrap(),
             job_files: Vec::new()
         }
+    }
+}
+
+impl Settings {
+    pub fn get_base_dir() -> Result<String, Box<dyn Error>> {
+        let data_dir = match ProjectDirs::from("com", "mycron", "mycron"){
+            Some(v) => v,
+            None => {
+                return Result::Err(String::from("Cannot find settings file in project directory").into());
+            }
+        };
+        let p = data_dir.data_dir().to_str().unwrap();
+        Ok(String::from(p))
+    }
+    
+    pub fn load_settings() -> Result<Settings, Box<dyn Error>> {
+        let data_dir = match ProjectDirs::from("com", "mycron", "mycron"){
+            Some(v) => v,
+            None => {
+                return Result::Err(String::from("Cannot find settings file in project directory").into());
+            }
+        };
+        let mut settings_path = PathBuf::from(data_dir.data_dir());
+        settings_path.push("settings.yaml");
+        let input_string = fs::read_to_string(&settings_path)?;
+        let s: Settings = serde_yaml_ng::from_str(&input_string)?;
+        return Ok(s);
+    }
+
+    pub fn save_settings(s: &Settings) -> Result<(), Box<dyn Error>> {
+        let data_dir = match ProjectDirs::from("com", "mycron", "mycron"){
+            Some(v) => v,
+            None => {
+                return Result::Err(String::from("Cannot find settings file in project directory").into());
+            }
+        };
+        let mut settings_path = PathBuf::from(data_dir.data_dir());
+        settings_path.push("settings.yaml");
+        let output_string = serde_yaml_ng::to_string(&s)?;
+        fs::write(settings_path, &output_string)?;
+        Ok(())
+    }
+
+    pub fn set_system_log(&mut self, p: &Path) -> Result<(), String> {
+        if p.exists() && p.is_file() {
+            self.system_log = String::from(p.to_str().unwrap());
+            return Ok(());
+        } else {
+            return Err(format!("Path: {} either is not valid or is not a file", p.to_str().unwrap()));
+        }
+    }
+
+    pub fn get_system_log(&self) -> String {
+        self.system_log.clone()
+    }
+
+    pub fn set_job_log(&mut self, p: &Path) -> Result<(), String> {
+        if p.exists() && p.is_dir() {
+            self.job_log = String::from(p.to_str().unwrap());
+            return Ok(());
+        } else {
+            return Err(format!("Path: {} either is not valid or is not a directory", p.to_str().unwrap()));
+        }
+    }
+
+    pub fn get_job_log(&self) -> String {
+        self.job_log.clone()
+    }
+
+    pub fn add_job_file(&mut self, p: &Path) -> Result<(), String> {
+        if p.exists() && p.is_file() {
+            self.job_files.push(String::from(p.to_str().unwrap()));
+            return Ok(());
+        } else {
+            return Err(format!("Path: {} either is not valid or is not a file", p.to_str().unwrap()));
+        }
+    }
+
+    pub fn remove_job_file(&mut self, i: usize) -> Result<(), String> {
+        if i < self.job_files.len() {
+            self.job_files.swap_remove(i);
+            return Ok(());
+        } else {
+            return Err(format!("Cannot remove job file. Index out of bounds"));
+        }
+    }
+
+    pub fn get_job_file(&self, i: usize) -> Result<String, String> {
+        if i < self.job_files.len() {
+            Ok(self.job_files.get(i).unwrap().clone())
+        } else {
+            return Err(format!("Cannot get job file. Index out of bounds"));
+        }
+    }
+
+    pub fn get_all_job_files(&self) -> Vec<String> {
+        self.job_files.clone()
     }
 }
